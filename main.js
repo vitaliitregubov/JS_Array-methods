e9048219f3aa0eb615127d0fb119347c17816fee
vitalii <tregubovvakharkov@gmail.com>
2021-03-13 22:40:00 +0200

    commit

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+"use strict";
 
+const tabControls = document.querySelectorAll(".tab-control");
+
+tabControls.forEach(tab => {
+  tab.addEventListener("click", e => tabSwitch(e))
+});
+
+function tabSwitch(e) {
+  alert(e.target.dataset.target);
+}
 
