297f130a77e741da027b8fd4ed670c01c7a67293
vitalii <tregubovvakharkov@gmail.com>
2021-03-13 23:34:13 +0200

    tabs

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 "use strict";
 
 const tabControls = document.querySelectorAll(".tab-control");
+const tabContents = document.querySelectorAll(".tab-content");
 
 tabControls.forEach(tab => {
-  tab.addEventListener("click", e => tabSwitch(e))
+  tab.addEventListener("click", ({ target }) => tabSwitch(target))
 });
 
-function tabSwitch(e) {
-  alert(e.target.dataset.target);
+function tabSwitch(el) {
+  const target = el.dataset.target;
+
+  tabControls.forEach(tab => {
+    tab.dataset.target === target 
+      ? tab.classList.add("active")
+      : tab.classList.remove("active")
+  });
+
+  tabContents.forEach(item => {
+    item.dataset.target === target 
+      ? item.classList.add("active")
+      : item.classList.remove("active")
+  })
 }
 
